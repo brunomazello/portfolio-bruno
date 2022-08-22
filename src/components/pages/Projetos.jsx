b24c4e1dc8a105f63824f6bf87fcefd68f0e8492
Bruno <bruno.rmazello@gmail.com>
2022-08-22 15:18:16 -0300

    fix(layout): Correção na margem direita da tela.

--- src/components/pages/Projetos.jsx
+++ src/components/pages/Projetos.jsx
@@ -3,7 +3,7 @@ import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Projetos = () => {
   return (
-    <div className="flex items-center justify-center bg-black">
+    <div className="flex items-center justify-center bg-black w-full">
       <AnimationOnScroll animateIn="animate__bounceIn">
         <div>
           <h1
