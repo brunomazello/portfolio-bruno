5dd56785cc2105e41f5327ad080f5b24d74bfd40
Bruno <bruno.rmazello@gmail.com>
2022-08-22 20:56:06 -0300

    feat(icons): Adicionado Facebook.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import { BsLinkedin, BsGithub, BsBehance } from "react-icons/bs";
+import { BsLinkedin, BsGithub, BsBehance, BsFacebook } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="flex justify-center">
@@ -8,7 +8,14 @@ const Footer = () => {
           target="_blank"
           rel="noopener"
         >
-          <BsLinkedin className="text-5xl text-white hover:text-blue-600 duration-500 hover:cursor-pointer hover:text-7xl mr-10" />
+          <BsLinkedin className="text-5xl text-white hover:text-sky-400 duration-500 hover:cursor-pointer hover:text-7xl mr-10" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/brunomazello/"
+          target="_blank"
+          rel="noopener"
+        >
+          <BsFacebook className="text-5xl text-white hover:text-blue-600 duration-500 hover:cursor-pointer hover:text-7xl mr-10" />
         </a>
         <a
           href="https://github.com/brunomazello/"
@@ -22,7 +29,7 @@ const Footer = () => {
           target="_blank"
           rel="noopener"
         >
-          <BsBehance className="text-5xl text-white duration-500 hover:text-pink-600 hover:cursor-pointer hover:text-7xl" />
+          <BsBehance className="text-5xl text-white duration-500 hover:text-pink-600 hover:cursor-pointer hover:text-7xl mr-10" />
         </a>
       </footer>
     </div>
