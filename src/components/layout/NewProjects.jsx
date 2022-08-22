8d4e3c843dfead3546ba2e9e52c6caaf5ad7a18e
Bruno <bruno.rmazello@gmail.com>
2022-08-22 15:27:55 -0300

    fix(layout): Corrigido diagramação e over de projetos.

--- src/components/layout/NewProjects.jsx
+++ src/components/layout/NewProjects.jsx
@@ -3,7 +3,7 @@ import projeto01 from "../../img/projeto_github.png";
 const NewProjects = () => {
   return (
     <div className="flex flex-wrap justify-items-center items-center mb-10 bg-black ml-10 mr-10">
-      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer sm:mr-14">
+      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer grayscale hover:grayscale-0  ">
         <img
           src={projeto01}
           alt=""
@@ -18,7 +18,7 @@ const NewProjects = () => {
           </p>
         </div>
       </div>
-      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer sm:mr-14">
+      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer grayscale hover:grayscale-0  ">
         <img
           src={projeto01}
           alt=""
@@ -33,12 +33,8 @@ const NewProjects = () => {
           </p>
         </div>
       </div>
-      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer ">
-        <img
-          src={projeto01}
-          alt=""
-          className="w-72 grayscale hover:grayscale-0 duration-300 rounded"
-        />
+      <div className="border rounded hover:border-green-500 duration-300 p-3 mt-10 hover:cursor-pointer grayscale hover:grayscale-0  ">
+        <img src={projeto01} alt="" className="w-72 rounded" />
         <div className="w-56 mt-5 flex items-center flex-wrap justify-center ml-12 mb-5">
           <h1 className="text-white font-bold hover:text-green-500">
             Consulta Github
