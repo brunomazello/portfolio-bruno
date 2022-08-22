f4e3b4dd230b1dc968d84a3ceaad9983b01f2fd2
Bruno <bruno.rmazello@gmail.com>
2022-08-21 22:50:30 -0300

    feat(Titulos): Adicionado titulo a página princial.

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -2,13 +2,17 @@ import imgbruno from "../../img/img_bruno.png";
 
 const Index = () => {
   return (
-    <div className="container">
-      <div className="">
+    <div className="">
+      <div className="flex justify-center items-center">
         <img
           src={imgbruno}
           alt=""
-          className="opacity-100 hover:opacity-50 hover:cursor-pointer"
+          className="opacity-100 hover:opacity-50 hover:cursor-pointer mb-12 mt-12 w-100 sd:w-60 mr-12"
         />
+        <div>
+          <h1 className="text-white font-mono text-7xl">Desenvolvedor Frontend</h1>
+          <h1 className="text-white font-sans">Desenvolvedor Frontend</h1>
+        </div>
       </div>
     </div>
   );
