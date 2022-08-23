e93a2336eb7cde3e2bda307142a9d95ebed37d88
Bruno <bruno.rmazello@gmail.com>
2022-08-22 21:31:47 -0300

    feat(codigo): codigo refatorado.

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,27 +11,27 @@ import {
 
 const Navbar = () => {
   return (
-    <nav class="relative flex flex-wrap items-center justify-between px-2 py-3mb-3">
-      <div class="container px-4 mx-auto flex flex-wrap items-center justify-between mt-3">
-        <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3mb-3">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between mt-3">
+        <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
           <a
-            class="text-sm font-bold text-2xl leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white hover:text-pink-600 hover:cursor-pointer hover:ease-in duration-300"
+            className="text-sm font-bold text-2xl leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white hover:text-pink-600 hover:cursor-pointer hover:ease-in duration-300"
             href="#"
           >
             Bruno Mazello
           </a>
         </div>
-        <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
-          <ul class="flex flex-col lg:flex-row list-none ml-auto">
-            <li class="nav-item">
+        <div className="lg:flex flex-grow items-center" id="example-navbar-warning">
+          <ul className="flex flex-col lg:flex-row list-none ml-auto">
+            <li className="nav-item">
               <a
-                class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300"
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300"
                 href="#"
               >
-                <span class="ml-2">Home</span>
+                <span className="ml-2">Home</span>
               </a>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link
                 to="projetos"
                 spy={true}
@@ -39,12 +39,12 @@ const Navbar = () => {
                 offset={50}
                 duration={500}
               >
-                <span class="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300">
+                <span className="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300">
                   Projetos
                 </span>
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link
                 to="contato"
                 spy={true}
@@ -52,7 +52,7 @@ const Navbar = () => {
                 offset={50}
                 duration={500}
               >
-                <span class="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300">
+                <span className="ml-2 px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:text-green-500 hover:cursor-pointer hover:ease-in duration-300">
                   Contato
                 </span>
               </Link>
